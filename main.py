69ced6bef582bc41634b37d4ecd80a20409b1413
gertberd <15979373+gertberd@users.noreply.github.com>
2021-01-12 12:09:06 +0300

    Удалил cогласование слова 'секунда' с числительными

--- main.py
+++ main.py
@@ -14,26 +14,24 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=10, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notify_progress(secs_left, bot, chat_id, message_id, seconds, matching_word):
-    matched_word = matching_word.make_agree_with_number(secs_left).word
+def notify_progress(secs_left, bot, chat_id, message_id, seconds):
     current_progress = render_progressbar(seconds, secs_left)
-    message = f'Осталось {secs_left} {matched_word}\n{current_progress}'
+    message = f'Осталось {secs_left} секунд\n{current_progress}'
     bot.update_message(chat_id, message_id, message)
     if not secs_left:
         message = 'Время вышло'
         bot.send_message(chat_id, message)
 
 
-def parse_message(message, bot, chat_id, matching_word):
+def parse_message(message, bot, chat_id):
     seconds = parse(message)
     if not seconds:
         bot.send_message(
             chat_id,
             'На сколько запустить таймер? (Примеры: 30s, 5m)')
         return
-    matched_word = matching_word.make_agree_with_number(seconds).word
-    message_id = bot.send_message(chat_id, f'Таймер запущен на {seconds} {matched_word}')
-    bot.create_countdown(seconds, notify_progress, bot=bot, chat_id=chat_id, message_id=message_id, seconds=seconds, matching_word=matching_word)
+    message_id = bot.send_message(chat_id, f'Таймер запущен на {seconds} секунд')
+    bot.create_countdown(seconds, notify_progress, bot=bot, chat_id=chat_id, message_id=message_id, seconds=seconds)
 
 
 def main():
@@ -43,9 +41,7 @@ def main():
     chat_id = env('TG_CHAT_ID')
     bot = Bot(bot_token)
     bot.send_message(chat_id, 'Бот запущен!')
-    morph = pymorphy2.MorphAnalyzer()
-    matching_word = morph.parse('секунда')[0]
-    bot.reply_on_message(parse_message, bot, chat_id, matching_word)
+    bot.reply_on_message(parse_message, bot, chat_id)
     bot.run_bot()
 
 
